d4683c28b9f666be477251cb3e4124a912fdaa02
Ryqite <garbaruk.901@gmail.com>
2024-10-28 20:00:52 +0300

    working version with authentication2

--- app/src/main/java/com/example/kursovaya/MainActivity.kt
+++ app/src/main/java/com/example/kursovaya/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.kursovaya
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.renderscript.ScriptGroup
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TableLayout
@@ -33,34 +35,61 @@ class MainActivity : AppCompatActivity() {
         binding.loginButtonContinue.setOnClickListener{
             var loginLogin: String = binding.loginText.getText().toString()
             var passwordLogin: String=binding.loginPasswordText.getText().toString()
-            startNewActivity(binding.loginButtonContinue)
+            //startNewActivity(binding.loginButtonContinue)
+            auth.signInWithEmailAndPassword(binding.loginText.getText().toString().trim(),
+                binding.loginPasswordText.getText().toString().trim())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+                        val intent = Intent(this, MainMenuActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
         }
         binding.singupButtonContinue.setOnClickListener{
             var loginSingup: String=binding.singupLoginText.getText().toString()
             var passwordSingup: String=binding.singupPasswordText.getText().toString()
             var passwordRepeatSingup: String=binding.singupPasswordRepeatText.getText().toString()
-            checkPasswords(passwordSingup,passwordRepeatSingup,binding.singupButtonContinue)
-        }
-    }
-    public override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        if (currentUser != null) {
-            val intent = Intent(this, MainMenuActivity::class.java)
-            startActivity(intent)
-        }
-    }
-    fun checkPasswords(passwordSingup: String,passwordRepeatSingup: String,v: View){
-        if(passwordSingup!=passwordRepeatSingup)
+            if(passwordSingup!=passwordRepeatSingup)
             {
                 Toast.makeText(this,"Passwords aren't same", Toast.LENGTH_SHORT).show()
             }
-        else if(passwordSingup.length<6){
-            Toast.makeText(this,"Password are less than 6 symbols", Toast.LENGTH_SHORT).show()
-        }
-        else{
-            startNewActivity(v)
+            else if(passwordSingup.length<6){
+                Toast.makeText(this,"Password are less than 6 symbols", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                auth.createUserWithEmailAndPassword(
+                    binding.singupLoginText.getText().toString().trim(),
+                    binding.singupPasswordText.getText().toString().trim()
+                )
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success")
+                            val user = auth.currentUser
+                            val intent = Intent(this, MainMenuActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                            Toast.makeText(
+                                baseContext,
+                                "Authentication failed.",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                        }
+                    }
+            }
         }
     }
     fun changeToLogin(v: View){
