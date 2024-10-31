0aca9346197a4d405ae8fff9dfbb505af18e90a5
Ryqite <garbaruk.901@gmail.com>
2024-10-31 20:52:44 +0300

    working version with completed realtime database(redisgned item)

--- app/src/main/java/com/example/kursovaya/ActivityDishPage.kt
+++ app/src/main/java/com/example/kursovaya/ActivityDishPage.kt
@@ -3,10 +3,8 @@ package com.example.kursovaya
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class activity_dish_page : AppCompatActivity() {
+class ActivityDishPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
